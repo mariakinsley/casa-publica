6a4febdd046cb49c116a1ae1cb669f23cabbbc6d
Maria Kinsley <mariadfrancia2016@gmail.com>
2017-05-22 18:47:52 -0400

    linking pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,27 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def dinner
+  end
+
+  def lunch
+  end
+
+  def brunch
+  end
+
+  def drinks
+  end
+
+  def reservations
+  end
+
+  def contact
+    puts('yo')
+  end
+
+  def map
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => "home#index"
+  get '/dinner' => "home#dinner"
+  get '/lunch' => "home#lunch"
+  get '/brunch' => "home#brunch"
+  get '/drinks' => "home#drinks"
+  get '/reservations' => "home#reservations"
+  get '/contact' => "home#contact"
+  get '/map' => "home#map"
 end
