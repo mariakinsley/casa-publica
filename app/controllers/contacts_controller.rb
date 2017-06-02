425c911ac85834cc9d83cabed7dfff42e3ed3711
Maria Kinsley <mariadfrancia2016@gmail.com>
2017-06-02 14:23:07 -0400

    dev db only

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,13 +4,13 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(params[:contact])
-    @contact.request = request
-    if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
-    else
-      flash.now[:error] = 'Cannot send message.'
-      render :new
-    end
+    # @contact = Contact.new(params[:contact])
+    # @contact.request = request
+    # if @contact.deliver
+    #   flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+    # else
+    #   flash.now[:error] = 'Cannot send message.'
+    #   render :new
+    # end
   end
 end
